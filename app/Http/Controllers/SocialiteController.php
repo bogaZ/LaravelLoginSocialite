1c4d6c5db56f81f9605163687dcfcb9be23f300a
bogaZ <bagusnurahmad7@gmail.com>
2023-01-25 10:56:43 +0700

    update error handling

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Dotenv\Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use TheSeer\Tokenizer\Exception;
 
 
 
@@ -19,17 +21,33 @@ class SocialiteController extends Controller
 
     public function callback($provider)
     {
-        $socialiteUser = Socialite::driver($provider)->user();
+        try {
+            $socialiteUser = Socialite::driver($provider)->user();
 
-        $user = User::updateOrCreate(
-            [
-                'name' => $socialiteUser->name,
-                'email' => $socialiteUser->email,
-                $provider . '_id' => $socialiteUser->id,
-            ]
-        );
+            $data = User::all()->firstWhere('google_id', $socialiteUser->id);
 
-        Auth::login($user);
-        return redirect()->route('home');
+            if (!empty($data)) {
+                Auth::login($data);
+                return redirect()->route('home');
+            }
+
+            $user = User::create(
+                [
+                    'name' => $socialiteUser->name,
+                    'email' => $socialiteUser->email,
+                    $provider . '_id' => $socialiteUser->id,
+                ]
+            );
+
+            Auth::login($user);
+
+            return redirect()->route('home');
+
+
+        } catch (\Exception $e) {
+            report($e);
+
+            return redirect()->route('login')->with('loginError', 'Login is not successfully, please Check your Email & Password');
+        }
     }
 }
